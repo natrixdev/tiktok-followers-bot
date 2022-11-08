1c21b95fb8b2078c0e75fca183fa9d70bb3cb11f
natrix_dev <discordlenko@gmail.com>
2022-11-08 21:35:09 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+# Modules
+
+import tiktok; import proxies; import creator: import account; import time 
+  
+# Creating accounts 
+
+from tiktok import accounts 
+from account import email, password, username 
+from proxies import * 
+
+creator.init(tiktok.com --accounts)
+def __accountCreator__:
+  x=creator.newAccount(tiktok, email, password, username) 
+    x.append()
+      x.changeAvatar(RandomPictureNet) 
+        x.subTo(MostFolloweds) 
+        
+  # Sub to your account
+  
+  def __subBot__   
+  
+  
+  # COMMING SOON ON GITHUB.COM/NATRIXDEV !!!
+  
